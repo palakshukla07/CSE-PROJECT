4206e54b5bf6bc006067fc3040f47208f123bfbb
Shatakshiguha <130547949+Shatakshiguha@users.noreply.github.com>
2023-05-22 14:42:39 +0530

    Update question 9.py

---
+++ question 9.py
@@ -0,0 +1,11 @@
+start
+|
+|__ Print "Enter initial pressure, volume, and final pressure"
+|   |
+|   |__ Accept initial pressure (P1), initial volume (V1), and final pressure (P2)
+|   |
+|   |__ Set V2 to (P1 * V1) / P2
+|   |
+|   |__ Print "The volume at pressure P2 is V2"
+|
+|__ End
